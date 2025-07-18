9d90c2d44fa8b639bc74a40b90818856eff4cec1
Herman Teng <herman.teng@outlook.com>
2025-07-18 08:19:55 -0400

    fix: add Unsplash image domains to next.config.js to resolve image loading errors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   images: {
     domains: [
       'images.pexels.com',
+      'images.unsplash.com',
+      'plus.unsplash.com',
       'i.ibb.co',
       'o.130014.xyz',
       'www.cathrinewilhelmsen.net',
